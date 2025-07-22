7698adc6b45d0db51e76feeae9d8a75fd411eb9f
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-07-22 16:45:27 +0530

    uu

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'class', // or 'media'
+  theme: {
+    extend: {
+      colors: {
+        lightbg: 'var(--color-light-bg)',
+        lighttext: 'var(--color-light-text)',
+        darkbg: 'var(--color-dark-bg)',
+        darktext: 'var(--color-dark-text)',
+      },
+    },
+  },
+  plugins: [],
+}
